841b71673aa2e6f96d08ecf4a83f3da2a0c0a97c
Mohamoud-Jire <amoh54932@gmail.com>
2023-07-18 06:38:41 +0300

    Answered

--- challenge.js
+++ challenge.js
@@ -1,2 +1,12 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function calculateBMI(weight, height) {
+  console.log(weight / (height * height));
+}
+let Ali = calculateBMI(79, 1.3);
+let Mohamed = calculateBMI(69, 1);
+if(Ali > Mohamed) {
+  console.log(`Ali's BMI is higher`);
+} else {
+  console.log(`Mohamed's BMI is higher`);
+}
